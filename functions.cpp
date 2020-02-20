0b81360b83e3cd1dfa2eade78c511734250b6bac
Veloraxzia <impendingdoom.taco@gmail.com>
2020-02-20 12:41:59 -0700

    Finished lab

--- functions.cpp
+++ functions.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <cmath>
 #include "header.hpp"
-using namespace std;
 
 Complex::Complex() {
 	real = 0;
+	imag = 0;
+}
+Complex::Complex(double x) {
+	real = x;
 	imag = 0;
 }
 Complex::Complex(double x, double y) {
@@ -15,11 +19,80 @@ Complex::Complex(const Complex &c) {
 	imag = c.imag;
 }
 void Complex::print() {
-	cout << "Real: " << real << "\nImag: " << imag << endl;
+	std::cout << this << std::endl;
 }
-Complex operator+(Complex c) {
+Complex Complex::add(Complex c) {
 	Complex temp;
 	temp.real = real + c.real;
 	temp.imag = imag + c.imag;
 	return temp;
+}
+Complex Complex::sub(Complex c) {
+	Complex temp;
+	temp.real = real - c.real;
+	temp.imag = imag - c.imag;
+	return temp;
+}
+Complex Complex::mult(Complex c) {
+	Complex temp;
+	temp.real = real * c.real;
+	temp.imag = imag * c.imag;
+	return temp;
+}
+Complex Complex::div(Complex c) {
+	if((c.real == 0) || (c.imag == 0)) {
+		std::cout << "n o" << std::endl;
+	}
+	Complex temp;
+	temp.real = real / c.real;
+	temp.imag = imag / c.imag;
+	return temp;
+}
+Complex Complex::con() {
+	Complex temp;
+	temp.imag = -1 * imag;
+	return temp;
+}
+double Complex::magnitude() {
+	return sqrt((real * real) + (imag * imag));
+}
+double Complex::phase() {
+	return atan(imag / real);
+}
+Complex Complex::operator+(Complex c) {
+	Complex temp = *this;
+	temp.add(c);
+	return temp;
+}
+Complex Complex::operator-(Complex c) {
+	Complex temp = *this;
+	temp.sub(c);
+	return temp;
+}
+Complex Complex::operator*(Complex c) {
+	Complex temp = *this;
+	temp.mult(c);
+	return temp;
+}
+Complex Complex::operator/(Complex c) {
+	Complex temp = *this;
+	temp.div(c);
+	return temp;
+}
+Complex Complex::operator=(Complex c) {
+	real = c.real;
+	imag = c.imag;
+	return *this;
+}
+std::ostream& operator<<(std::ostream &out, const Complex &c) {
+	double temp;
+	if(c.imag < 0) {
+		temp = -1 * c.imag;
+		out << c.real << " - " << c.imag << "i";
+	} else {
+		out << c.real << " + " << c.imag << "i";
+	}
+}
+std::istream& operator>>(std::istream &in, Complex &c) {
+	in >> c.real >> c.imag;
 }
--- header.hpp
+++ header.hpp
@@ -6,14 +6,22 @@ class Complex {
 		double imag;
 	public:
 		Complex();
-		Complex(double, double);
-		Complex::Complex(const Complex &c);
-		void Complex::print();
+		Complex(double x);
+		Complex(double x, double y);
+		Complex(const Complex &c);
+		void print();
+		Complex add(Complex c);
+		Complex sub(Complex c);
+		Complex mult(Complex c);
+		Complex div(Complex c);
+		Complex con();
+		double magnitude();
+		double phase();
 		Complex operator+(Complex c);
 		Complex operator-(Complex c);
 		Complex operator*(Complex c);
 		Complex operator/(Complex c);
-		Complex conjugate(Complex c);
-		double magnitude(Complex c);
-		double phase(Complex c);
+		Complex operator=(Complex c);
+		friend std::ostream& operator<<(std::ostream &out, const Complex &c);
+		friend std::istream& operator>>(std::istream &in, Complex &c);
 };
