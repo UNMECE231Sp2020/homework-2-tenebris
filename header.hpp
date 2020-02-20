3e4bfc0913b4c769f6338f27312f715abc258b3c
Veloraxzia <impendingdoom.taco@gmail.com>
2020-02-20 11:23:56 -0700

    Finished header

--- header.hpp
+++ header.hpp
@@ -10,4 +10,10 @@ class Complex {
 		Complex::Complex(const Complex &c);
 		void Complex::print();
 		Complex operator+(Complex c);
+		Complex operator-(Complex c);
+		Complex operator*(Complex c);
+		Complex operator/(Complex c);
+		Complex conjugate(Complex c);
+		double magnitude(Complex c);
+		double phase(Complex c);
 };
