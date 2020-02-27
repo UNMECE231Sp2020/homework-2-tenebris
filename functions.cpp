fd83b26fa14e47eb4d2e42e6562e05fbcd620327
Veloraxzia <impendingdoom.taco@gmail.com>
2020-02-26 20:16:53 -0700

    Edited to fit coding standard and added getters

--- functions.cpp
+++ functions.cpp
@@ -6,52 +6,57 @@ Complex::Complex() {
 	real = 0;
 	imag = 0;
 }
-Complex::Complex(double x) {
-	real = x;
-	imag = 0;
+Complex::Complex(double _real) {
+	*this = Complex(_real, 0);
 }
-Complex::Complex(double x, double y) {
-	real = x;
-	imag = y;
+Complex::Complex(double _real, double _imag) {
+	real = _real;
+	imag = _imag;
 }
-Complex::Complex(const Complex &c) {
-	real = c.real;
-	imag = c.imag;
+Complex::Complex(const Complex &comp) {
+	real = comp.real;
+	imag = comp.imag;
+}
+double Complex::getReal() {
+	return this->real;
+}
+double Complex::getImag() {
+	return this->real;
 }
 void Complex::print() {
 	std::cout << *this << std::endl;
 }
-Complex Complex::add(Complex c) {
+Complex Complex::add(Complex comp) {
 	Complex temp;
-	temp.real = real + c.real;
-	temp.imag = imag + c.imag;
+	temp.real = real + comp.real;
+	temp.imag = imag + comp.imag;
 	return temp;
 }
-Complex Complex::sub(Complex c) {
+Complex Complex::sub(Complex comp) {
 	Complex temp;
-	temp.real = real - c.real;
-	temp.imag = imag - c.imag;
+	temp.real = real - comp.real;
+	temp.imag = imag - comp.imag;
 	return temp;
 }
-Complex Complex::mult(Complex c) {
+Complex Complex::mult(Complex comp) {
 	Complex temp;
-	temp.real = (real * c.real) - (imag * c.imag);
-	temp.imag = (real * c.imag) + (imag * c.real);
+	temp.real = (real * comp.real) - (imag * comp.imag);
+	temp.imag = (real * comp.imag) + (imag * comp.real);
 	return temp;
 }
-Complex Complex::div(Complex c) {
-	if((c.real == 0) && (c.imag == 0)) {
+Complex Complex::div(Complex comp) {
+	if((comp.real == 0) && (comp.imag == 0)) {
 		std::cout << "n o" << std::endl;
 	}
 	Complex temp;
-	double div = (c.real * c.real) + (c.imag * c.imag);
-	temp.real = (real * c.real) + (imag * c.imag);
-	temp.imag = (real * c.imag) - (imag * c.real);
+	double div = (comp.real * comp.real) + (comp.imag * comp.imag);
+	temp.real = (real * comp.real) + (imag * comp.imag);
+	temp.imag = (real * comp.imag) - (imag * comp.real);
 	temp.real /= div;
 	temp.imag /= div;
 	return temp;
 }
-Complex Complex::con() {
+Complex Complex::conj() {
 	Complex temp;
 	temp.imag = -1 * imag;
 	return temp;
@@ -62,33 +67,33 @@ double Complex::magnitude() {
 double Complex::phase() {
 	return atan(imag / real) * 180 / M_PI;
 }
-Complex Complex::operator+(Complex c) {
-	return add(c);
+Complex Complex::operator+(Complex comp) {
+	return add(comp);
 }
-Complex Complex::operator-(Complex c) {
-	return c.sub(*this);
+Complex Complex::operator-(Complex comp) {
+	return comp.sub(*this);
 }
-Complex Complex::operator*(Complex c) {
-	return mult(c);
+Complex Complex::operator*(Complex comp) {
+	return mult(comp);
 }
-Complex Complex::operator/(Complex c) {
-	return div(c);
+Complex Complex::operator/(Complex comp) {
+	return div(comp);
 }
-Complex Complex::operator=(Complex c) {
-	real = c.real;
-	imag = c.imag;
+Complex Complex::operator=(Complex comp) {
+	real = comp.real;
+	imag = comp.imag;
 	return *this;
 }
-std::ostream& operator<<(std::ostream &out, const Complex &c) {
-	if(c.imag < 0) {
-		double temp = -1 * c.imag;
-		out << c.real << " - " << temp << "i";
+std::ostream& operator<<(std::ostream &out, const Complex &comp) {
+	if(comp.imag < 0) {
+		double temp = -1 * comp.imag;
+		out << comp.real << " - " << temp << "i";
 	} else {
-		out << c.real << " + " << c.imag << "i";
+		out << comp.real << " + " << comp.imag << "i";
 	}
 	return out;
 }
-std::istream& operator>>(std::istream &in, Complex &c) {
-	in >> c.real >> c.imag;
+std::istream& operator>>(std::istream &in, Complex &comp) {
+	in >> comp.real >> comp.imag;
 	return in;
 }
